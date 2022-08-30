e6cf0709c95ad7ac11a424cb273e29b1e155f414
Zeko <41507889+ZekoTheFox@users.noreply.github.com>
2022-08-29 20:04:59 -0700

    Add offline page.

---
+++ src/routes/discord/+page.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+    <title>offline - zeko.party</title>
+</svelte:head>
+
+<div class="page">
+    this link may be offline, or the redirect is down for the moment.
+</div>
+
+<style>
+    .page {
+        height: 100vh;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
